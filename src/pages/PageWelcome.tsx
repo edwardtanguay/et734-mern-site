ff11fbb2d67874429e86d199bbe12291086710e6
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-22 11:29:53 +0100

    getting 9 employees

--- src/pages/PageWelcome.tsx
+++ src/pages/PageWelcome.tsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IEmployee } from "../interfaces";
+import * as config from "../config";
+import axios from "axios";
 
 export const PageWelcome = () => {
 	const [employees, setEmployees] = useState<IEmployee[]>([]);
+
+	useEffect(() => {
+		(async () => {
+			const response = await axios.get(
+				`${config.backendURL()}/employees`
+			);
+			const _employees = response.data;
+			setEmployees(_employees);
+		})();
+	}, []);
 
 	return (
 		<>
 			<h2>There are {employees.length} employees:</h2>
 		</>
-	)
-}
+	);
+};
